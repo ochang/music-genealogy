9d9f9a3f3e2f1623b4028fd147c6a46af047eb75
Oliver Chang <oychang1@gmail.com>
2012-08-09 22:06:10 -0400

    works for one sublevel! need to fix line 78 I think

--- parser.py
+++ parser.py
@@ -36,7 +36,7 @@ genealogy = []
 for genre in genre_soup:
     # within h3 this span describes the big bold visible heading
     genre_name = genre.find_all('span', {'class':'mw-headline'})
-    genre_name = genre_name[0].string
+    genre_name = get_string(genre_name[0])
 
     # find the grouping that describes all the sub-genres
     # i.e. the top level, the first group of <li>
@@ -44,24 +44,43 @@ for genre in genre_soup:
         genre = genre.next_sibling
         # can be held in next div if many or ul if few
         try:
-            # if genre.name == 'div':
-            #     subgenres = genre.select('ul > li')
-            #     break
-            # elif genre.name == 'ul':
-            #     subgenres = genre.select('li')
-            #     break
-            if (genre.name == 'div') or (genre.name == 'ul'):
+            if genre.name == 'div':
+                subgenres = genre.select('ul > li')
                 subgenres = genre.find_all('li')
                 break
+            elif genre.name == 'ul':
+                subgenres = genre.find_all('li')
+                break
+        # if next sibling is not a div or ul
         except AttributeError: pass
 
-
     # we now have the chunk of contents that corresponds to genre_name stored in subgenres
-    for li in subgenres:
-        # parent_child = find_parent(li)
-        genealogy.append(get_string(li))
+    if genre_name == 'Country':
+        for li in subgenres:
+            li_name = get_string(li)
+            print 'Node name:' + li_name
+            if li_name == 'Bluegrass': break
 
-    if genre_name == 'Asian': break
+            while True:
+                li = li.parent
+                # print get_string(li)
 
-with codecs.open('draft_out.txt', 'w', 'utf-8-sig') as z:
-    z.write(str(genealogy))
+                yyy = li.previous_sibling
+                if yyy == '\n':
+                    zzz = yyy.previous_sibling
+
+                if zzz != None:
+                    print 'parent:'
+                    if zzz.name == 'a':
+                        print get_string(zzz)
+                    else:
+                        # we are already at top sublevel!
+                        print genre_name
+                    break
+
+            print '============'
+    elif genre_name == 'Easy listening':break
+
+
+# with codecs.open('draft_out.txt', 'w', 'utf-8-sig') as z:
+#     z.write(str(genealogy))
